d636f1fdbd7b8b3d9b3275635745f5de3cd514d4
LoEnzo <LoEnzo@users.noreply.github.com>
2020-08-23 10:35:22 +0000

    Automated deployment: Sun Aug 23 10:35:22 UTC 2020 0c457f9931f30d2c4ff8a16607b612ec4c2b24ce

--- js/ProjectsData.js
+++ js/ProjectsData.js
@@ -54,58 +54,16 @@ export default [
       thumbnail: 'https://s1.ax1x.com/2020/05/16/Y6BMHs.png'
     },
     { 
-      name: 'SUMMER-1874', 
-      desc: '当我沉默的时候，我觉得很充实，当我开口说话，就感到了空虚。',
-      link: 'http //summer1874.top', 
-      thumbnail: 'https //pic.downk.cc/item/5eb8fb6ac2a9a83be5e9d6f2.png'
-    },
-    { 
-      name: '海边的小溪鱼', 
-      desc: 'Study Programs And Record Life',
-      link: 'https://www.gaosanshi.top/', 
-      thumbnail: 'https://www.gaosanshi.top/head.png'
-    },
-    { 
       name: `shanks's blog`, 
       desc: '淡泊以明志，宁静而致远',
       link: 'https://www.mhynet.cn', 
       thumbnail: 'https://image.mhynet.cn/20200422202702.jpg' 
     },
     { 
-      name: '有梦想的咸鱼', 
-      desc: '我不能克制我记几',
-      link: 'https://blog.liudongyang.top/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/hc8xWH4kJyEZeSM.png' 
-    },
-    { 
       name: 'Vm Blog', 
       desc: '淡然Vm的个人博客',
       link: 'https://blog.danran.site/', 
       thumbnail: 'https://i.loli.net/2020/04/04/sMrJaqz1pcS5BGo.png' 
-    },
-    { 
-      name: '404 NOT FOUND', 
-      desc: '前端杂货铺',
-      link: 'https://wang.icey.cc/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/zByhlLKiQZgcCnU.png' 
-    },
-    { 
-      name: 'X · M', 
-      desc: 'Valar Morghulis ,Valar Dohaeris .',
-      link: 'http://yvonnecheung.cn/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/yIeWj8CLKDo7HN4.png' 
-    },
-    { 
-      name: '凝弧', 
-      desc: '理解了目标，方向自然就出现了',
-      link: 'https://callsoul.cn/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/4sxpmhZTula3D7t.png' 
-    },
-    { 
-      name: 'Leecason', 
-      desc: '若我所见不同，独守一角青空',
-      link: 'https://leecason.github.io', 
-      thumbnail: 'https://i.loli.net/2020/04/04/utP65sSnzULdGCb.png' 
     },
     { 
       name: '平凡的你我', 
@@ -132,58 +90,10 @@ export default [
       thumbnail: 'https://i.loli.net/2020/04/04/ebr4quEHgyC9n5W.png' 
     },
     { 
-      name: 'Black House', 
-      desc: '代码千万行, 注释第一行, 编码不规范, 同事两行泪.',
-      link: 'https://dong4j.github.io/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/PNUm1zfVCLF3eIb.png' 
-    },
-    { 
-      name: '学海无涯', 
-      desc: '暂无',
-      link: 'https://www.xuminhua.com/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/Nh9u5Bm1E8wniVz.png' 
-    },
-    { 
-      name: 'znote', 
-      desc: '荷尽已无擎雨盖，菊残犹有傲霜枝。',
-      link: 'https://zpj80231.github.io/znote/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/2Fq8jYSb6EkUXeH.png' 
-    },
-    { 
-      name: '凯小默的博客', 
-      desc: 'THE PHILOSOPHERS HAVE ONLY INTERPRETED THE WORLD IN VARIOUS WAYS;THE POINT HOWEVER IS TO CHANGE IT.',
-      link: 'https://kaimo313.github.io/kxm_blog/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/tqFPfXDRrT1VOmZ.png' 
-    },
-    { 
-      name: 'Coding and Fixing', 
-      desc: '源于生活而高于生活',
-      link: 'https://vxhly.gitee.io/blog/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/e5jaYcrhzxXJBNt.png' 
-    },
-    { 
-      name: '会走路的三百块', 
-      desc: '年轻人的博客',
-      link: 'https://castleeee.github.io/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/jqv5upVbkEDiTJP.png' 
-    },
-    { 
       name: 'Life\'s Moment', 
       desc: '记录生活，分享感动',
       link: 'https://ty-peng.gitee.io/', 
       thumbnail: 'https://i.loli.net/2020/04/04/srGnE4HXAIfPJ1N.png' 
-    },
-    { 
-      name: 'jokervTv 爬坑历程', 
-      desc: '大气科学 爬坑历程',
-      link: 'https://jokervtv.top/blog/', 
-      thumbnail: 'https://i.loli.net/2020/04/04/w2dRfYat3ybOj9G.png' 
-    },
-    {
-      name: '青灯有味',
-      desc: '一款简洁易懂算法博客 & 文档抠细节 & 主题',
-      link: 'https://zscnb.gitee.io',
-      thumbnail: 'https://zscnb.gitee.io/bg.png'
     },
     {
       name: 'Hexo中文站',
