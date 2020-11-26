78226ea88777d2e0701fdc7330bff45e58f7aa17
hjwu <hjwu@travelsky.com>
2020-11-26 11:21:26 +0800

    测试PWA

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -35,7 +35,43 @@ module.exports = {
     }],
   ],
   serviceWorker: true,
-  plugins: pluginConf,
+  plugins: [
+    [
+      'vuepress-plugin-sponsor',
+      {
+        theme: 'simple',
+        alipay: '/reward/alipay.JPG',
+        wechat: '/reward/wechatpay.JPG',
+        qq: '',
+        paypal: '',
+        duration: 2000
+      }
+    ],
+    // google网站分析
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-182914025-1' // UA-00000000-0
+      }
+    ],
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: {
+        message: "发现新内容可用",
+        buttonText: "刷新"
+      }
+    }],
+    '@vuepress-reco/extract-code',
+    'flowchart',
+    ['sitemap', {
+      hostname: 'https:/hjwu.gq'
+    }],
+    // 添加rss订阅插件，测试发现只能订阅标题和tip内容
+    ['@vuepress-reco/rss', {
+      site_url: 'https://hjwu.gq',
+      copyright: ''
+    }],
+  ],
   theme: "reco", 
   themeConfig: themeConf,
   markdown: {
