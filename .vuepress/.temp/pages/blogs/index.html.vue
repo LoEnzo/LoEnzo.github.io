def37646411842f971533228b4d4e90993809efa
loenzo <928264247@qq.com>
2023-12-14 13:44:02 +0800

    update .gitignore

--- .vuepress/.temp/pages/blogs/index.html.vue
+++
@@ -1,6 +0,0 @@
-<template><div><h2 id="参考" tabindex="-1"><a class="header-anchor" href="#参考" aria-hidden="true">#</a> 参考</h2>
-<h3 id="一款简介的vuepress博客主题vuepress-theme-reco" tabindex="-1"><a class="header-anchor" href="#一款简介的vuepress博客主题vuepress-theme-reco" aria-hidden="true">#</a> 一款简介的vuepress博客主题<a href="https://www.npmjs.com/package/vuepress-theme-reco" target="_blank" rel="noopener noreferrer">vuepress-theme-reco</a></h3>
-<h3 id="recoluan-s-blog-website" tabindex="-1"><a class="header-anchor" href="#recoluan-s-blog-website" aria-hidden="true">#</a> recoluan's blog <a href="https://recoluan.gitlab.io" target="_blank" rel="noopener noreferrer">website</a></h3>
-</div></template>
-
-
--- blogs/README.md
+++
@@ -1,5 +0,0 @@
-## 参考 
-
-### 一款简介的vuepress博客主题[vuepress-theme-reco](https://www.npmjs.com/package/vuepress-theme-reco)
-
-### recoluan's blog [website](https://recoluan.gitlab.io)
