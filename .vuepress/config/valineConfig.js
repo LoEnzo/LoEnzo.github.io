ab823e681dddf1a1d9d6ad3d4a0ca980c07941b1
LoEnzo <928264247@qq.com>
2023-12-21 21:23:14 +0800

    Update valineConfig.js
    更valine appid配置

--- .vuepress/config/valineConfig.js
+++ .vuepress/config/valineConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     type: 'valine',
     // options 与 1.x 的 valineConfig 配置一致
     options: {
-      appId: 'J1Uq1gtVn91RhSSw4FaAv2GD',
+      appId: 'J1Uq1gtVn91RhSSw4FaAv2GD-MdYXbMMI',
       appKey: 'rxJmmc3H8KmkmMeddXkBRnsw',
       placeholder: '支持markdown语法',
       // verify: true, // 验证码服务
